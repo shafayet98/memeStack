50054959e8a53ada55a2209abef5e02548e2f90a
Shafayet Ul Islam <35225819+shafayet98@users.noreply.github.com>
2020-09-17 09:42:29 +0600

    added meme_detection folder

--- mainproject/memes/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed
-
-class MemePostForm(FlaskForm):
-    meme_caption = TextAreaField("Meme Caption",validators=[DataRequired()])
-    meme_image =  FileField("Update Profile Pictures", validators= [FileAllowed(['jpg','jpeg','png'])])
-    submit = SubmitField("Post MEME")
