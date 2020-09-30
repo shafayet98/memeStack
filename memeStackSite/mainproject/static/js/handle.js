94e9a242e3967cfe418625428c3b8d6b29d99cb7
Shafayet <shafayet.islam.61@gmail.com>
2020-09-30 13:51:37 +0600

    form style added...

---
+++ memeStackSite/mainproject/static/js/handle.js
@@ -0,0 +1,20 @@
+//general
+var windowHeight = $(window).height();
+var getURL = window.location.href;
+
+// in login
+if (getURL.includes('login')) {
+    $(".navbar-brand").css("display", "none");
+    $(".about").css("display", "none");
+}
+
+// in index and profile for tooltip
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+// in register
+if (getURL.includes('register')) {
+    $(".navbar-brand").css("display", "none");
+    $(".about").css("display", "none");
+}
